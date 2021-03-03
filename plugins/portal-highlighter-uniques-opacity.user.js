260be5ddf71138a59cd2bdf6edbce4ec5e42b165
Charlie C. F. Lok <43951075+charlielok@users.noreply.github.com>
2021-03-03 17:55:11 +0800

    Update portal-highlighter-uniques-opacity.user.js

--- plugins/portal-highlighter-uniques-opacity.user.js
+++ plugins/portal-highlighter-uniques-opacity.user.js
@@ -13,6 +13,21 @@
 // @grant          none
 // ==/UserScript==
 
+
+function wrapper(plugin_info) {
+// ensure plugin framework is there, even if iitc is not yet loaded
+if(typeof window.plugin !== 'function') window.plugin = function() {};
+
+//PLUGIN AUTHORS: writing a plugin outside of the IITC build environment? if so, delete these lines!!
+//(leaving them in place might break the 'About IITC' page or break update checks)
+plugin_info.buildName = 'xificurk';
+plugin_info.dateTimeVersion = '20210207.174711';
+plugin_info.pluginId = 'portal-highlighter-uniques-opacity';
+//END PLUGIN AUTHORS NOTE
+
+
+//PLUGIN START ////////////////////////////////////////////////////////
+
 //use own namespace for plugin
 window.plugin.portalHighlighterUniquesOpacity = function () {};
 
@@ -57,3 +72,19 @@ var setup = function() {
 
   window.addPortalHighlighter('Uniques (opacity)', window.plugin.portalHighlighterUniquesOpacity.highlighter);
 }
+
+//PLUGIN END //////////////////////////////////////////////////////////
+
+
+setup.info = plugin_info; //add the script info data to the function as a property
+if(!window.bootPlugins) window.bootPlugins = [];
+window.bootPlugins.push(setup);
+// if IITC has already booted, immediately run the 'setup' function
+if(window.iitcLoaded && typeof setup === 'function') setup();
+} // wrapper end
+// inject code into site context
+var script = document.createElement('script');
+var info = {};
+if (typeof GM_info !== 'undefined' && GM_info && GM_info.script) info.script = { version: GM_info.script.version, name: GM_info.script.name, description: GM_info.script.description };
+script.appendChild(document.createTextNode('('+ wrapper +')('+JSON.stringify(info)+');'));
+(document.body || document.head || document.documentElement).appendChild(script);
